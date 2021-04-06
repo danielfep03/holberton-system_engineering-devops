f9cac8365ffda9bd8bd7ad7156a1b49b278abab0
Daniel Amado <danielfep.am@gmail.com>
2021-04-05 23:59:07 -0500

    [UPDATED] spaces fixed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,10 @@
-# Append to a file
-file_line { 'Append to a file':
+# Check for some lines in /etc/ssh_config, if doesn't exist, then create it.
+file_line { 'Disallow password auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no'
 }
-
-file_line { 'Append to a file':
+file_line { 'Use the private key ~/.ssh/holberton':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/holberton'
