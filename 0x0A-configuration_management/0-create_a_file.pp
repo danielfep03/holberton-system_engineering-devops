5aae31d05eed6e00ea9c5c3d5d46a1e63eee1dec
Daniel Amado <danielfep.am@gmail.com>
2021-04-05 22:13:43 -0500

    [UPDATED] Hot fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a file in /tmp
 file { '/tmp/holberton':
     ensure  => 'present',
-    mode    => '0774',
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet',
