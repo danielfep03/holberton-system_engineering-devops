a12cc866086c4e7006c8579ebc427618eb901d08
Daniel Amado <danielfep.am@gmail.com>
2021-05-03 23:19:51 -0500

    [UPDATED] Order of libraries changed

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 '''Script that use an API to show information about his Todo list progress'''
+
+import json
 import requests
 from sys import argv
-import json
 
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
@@ -21,5 +22,6 @@ if __name__ == '__main__':
         task['completed'] = todo['completed']
         task['username'] = employee[0]['username']
         my_dict[argv[1]].append(task)
+
     with open('{}.json'.format(argv[1]), mode='w', encoding='utf-8') as file:
         json.dump(my_dict, file)
