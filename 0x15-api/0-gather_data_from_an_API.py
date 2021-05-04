6c55c53dd86dfd7fb4a8a39c0cbebd0cf6daf720
Daniel Amado <danielfep.am@gmail.com>
2021-05-03 19:13:44 -0500

    [UPDATED] Fixed order libraries imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Script that use an API to show information about his Todo list progress'''
-from sys import argv
 import requests
+from sys import argv
 
 
 if __name__ == '__main__':
